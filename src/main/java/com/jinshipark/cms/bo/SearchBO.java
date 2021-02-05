f5c625bc3b6b18d3cb2dd922b7c45ced902370cb
dingjing <13962104109@163.com>
2021-02-05 17:18:03 +0800

    在场记录和在场记录历史接口

---
+++ src/main/java/com/jinshipark/cms/bo/SearchBO.java
@@ -0,0 +1,80 @@
+package com.jinshipark.cms.bo;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SearchBO {
+    private static DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private Integer pageNum;
+    private Integer pageSize;
+    private String parkId;
+    private String keyWord;
+    private String startTime;
+    private String endTime;
+    private String lpOrderState;
+    public Integer getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(Integer pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public String getParkId() {
+        return parkId;
+    }
+
+    public void setParkId(String parkId) {
+        this.parkId = parkId;
+    }
+
+    public String getKeyWord() {
+        return keyWord;
+    }
+
+    public void setKeyWord(String keyWord) {
+        this.keyWord = keyWord;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public Date getDateForStartTime() throws ParseException {
+        return format.parse(this.startTime.toString() + " 00:00:00");
+    }
+
+    public Date getDateForEndTime() throws ParseException {
+        return format.parse(this.endTime.toString() + " 23:59:59");
+    }
+
+    public String getLpOrderState() {
+        return lpOrderState;
+    }
+
+    public void setLpOrderState(String lpOrderState) {
+        this.lpOrderState = lpOrderState;
+    }
+}
